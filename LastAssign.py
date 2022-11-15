171cd1e101d95721c6e7863a19defa3a3a4330f6
Nisha Kafley <Kafny002@mymail.unisa.edu.au>
2022-11-15 20:24:50 +1030

    All the classes are created and for now I just passed the values. I also added comment and docstring for all the classes and methods.

--- LastAssign.py
+++ LastAssign.py
@@ -8,21 +8,28 @@
 # the University's Academic Misconduct Policy.
 # 
 
-
-class all_That_Dice:
+'''The all_That_Dice class runs the game, this class accesses the player
+class to add players to the game.'''
+class allThatDice:
+    '''The run method shows all the possible options. 
+    The run method also validates the player input.
+    '''
     def run():
         print("Welcome to All-That-Dice") 
         print("Developed by Alan Turning")
         print("COMP 1048 Object-Oriented Programming")
-        
+
+    '''This method will display all the options
+    It will also prompt and validate user choices
+    '''
+    def menu():
         print("\nWhat would you like to do?")
         print("(r) register a new player")
         print("(s) show the leader board")
         print("(p) play a game")
         print("(q) quit")
-        #made changes
 
-        # prompts the player to choose an option
+         # prompts the player to choose an option
         userChoice = input("\nWhat would you like to do?")
 
         #validates the users choice 
@@ -41,21 +48,44 @@ class all_That_Dice:
                 print("Showing the leader board")
             elif (userChoice == 'p'):
                 print("Playing a game")
-                
             return userChoice
 
-# This player class will register all the new players 
-# playerList[] will add all the names of the new players into a list 
+    '''This method will display all three game options. 
+    It will also prompt the user to choose of the the option'''
 
+    def gameOption():
+        print("\nWhich game would you like to play?")
+        print("(o) Odd-or-Even")
+        print("(m) Maxi")
+        print("(b) Bunco")
+
+        choice = input("\nWhat would you like to do?")
+
+        if choice == "o":
+           pass
+
+        elif choice == "m":
+            pass
+
+        if choice == "b":
+           pass
+
+
+
+'''This player class will register all the new players.
+ playerList[] will add all the names of the new players into a list 
+'''
 class players:
     def __init__(self):
         self.playerList = []   
 
+        
+    '''This method prompts the player to enter the name
+    It also validates if the new player name exists in playerList
+    If the playerName exists, then the player is asked to re-enter'''
+    
     def getPlayerName(self):
         newPlayer = input("\nWhat is the name of the new player? ")
-        # validates if the new player name exists in playerList
-        # if the playerName exists, then the player is asked to re-enter their name
-
         
         while newPlayer not in self.playerList:
             self.playerList.append(newPlayer)
@@ -64,17 +94,87 @@ class players:
 
         for newPlayer in self.playerList:
             print("Welcome", newPlayer)
-        
+    
 
         if newPlayer in self.playerList:
             print("\nSorry, the name is already taken")
             
 
 
-atd = all_That_Dice
+# Leaderboard class will show the results which inclues the namee, games played, won, and chips. 
+'''class leaderboard:
+    def __init__(self, Playername, gamePlayed, gameWon, chips):
+        self.playerName = Playername 
+        self.gamePlayed = gamePlayed
+        self.gameWon = gameWon
+        self.chips = chips'''
+    
+    
+    
+
+'''class diceGame:
+    def __init__(self, players, minPlayers, maxPlayers, chipsToBid):
+        self.players = players 
+        self.minPlayers = minPlayers
+        self.minPlayers = maxPlayers
+        self.chipsToBid = chipsToBid
+    
+    def playerGame(self):'''
+
+
+
+
+#played by 2-4 playees 
+# If the registered players are not enough, then an error message will display and players will be redirected to main menu
+# players name must match the registered name, if not error message will display and players are asked to re-enter thier name
+# Players will bid the chips
+
+class bunco():
+    pass 
+
+
+# played by 3-5 players 
+# Each player will roll a pair of dice
+# The player with the higest number of face up values of the dice will win the game 
+# If both players have the same higest die value, then they will play the game again 
+
+class maxi():
+    pass
+
+
+
+
+# Odd or Even only has 1 player
+# The player has to guess if the die will be odd or even.
+# If the player's guess corrosponds with the die, then the player wins
+
+class oddOrEven():
+
+    #def __init__(self, guess):
+        #self.__guess = guess 
+    
+    #def getGuess(self):
+        
+        
+        
+
+
+
+
+#class Die:
+    #pass
+
+
+atd = allThatDice
 atd.run()
+
+m = allThatDice
+m.menu()
 
 p = players()
 p.getPlayerName()
+
+g = allThatDice()
+g.gameOption()
 
 
